7d61b98cdba33288fdeeed0af78c36bccd8aed0b
Oğuzhan SARI <oguzn.sari@gmail.com>
2023-10-15 15:42:48 +0300

    fixed yearly summary and added wholeTime summary endpoint

--- api/controllers/vendorController.js
+++ api/controllers/vendorController.js
@@ -71,7 +71,12 @@ const getMonthlyProductDetails = async (req, res) => {
   }
 
   // Get an array of product IDs
-  const productIds = products.map((product) => product._id);
+  let productNames = [];
+  let productIds = [];
+  products.forEach((product) => {
+    productNames[product._id.toString()] = product.name;
+    productIds.push(product._id);
+  });
 
   // Create a pipeline to aggregate the data
   const pipeline = [
@@ -90,6 +95,7 @@ const getMonthlyProductDetails = async (req, res) => {
     {
       $match: {
         "cart_item.order_status": orderStatus, // Filter
+        "cart_item.product": { $in: productIds },
       },
     },
     {
@@ -98,6 +104,7 @@ const getMonthlyProductDetails = async (req, res) => {
           order_status: "$cart_item.order_status",
           month: { $month: "$payment_at" },
           year: { $year: "$payment_at" },
+          product: "$cart_item.product",
         },
         count: { $sum: 1 }, // Count
         totalPackQuantity: { $sum: "$cart_item.quantity" }, // Total quantity of products
@@ -106,7 +113,6 @@ const getMonthlyProductDetails = async (req, res) => {
         }, // Total individual orders
         totalCOG: { $sum: "$cart_item.cogs" }, // Total COG
         totalPrice: { $sum: "$cart_item.price" }, // Total price
-        productsOrdered: { $addToSet: "$cart_item.product" }, // Collect unique product IDs ordered in this month
       },
     },
     {
@@ -121,24 +127,166 @@ const getMonthlyProductDetails = async (req, res) => {
         totalCOG: 1,
         totalPrice: 1,
         totalRevenue: { $subtract: ["$totalPrice", "$totalCOG"] },
-        productsOrdered: 1,
+        product: "$_id.product",
       },
     },
   ];
 
-  // Perform a lookup to retrieve product names based on product IDs
-  pipeline.push({
-    $lookup: {
-      from: "products", // Replace with the actual name of the product collection
-      localField: "productsOrdered",
-      foreignField: "_id",
-      as: "productDetails",
-    },
+  const result = await Order.aggregate(pipeline);
+
+  // organize data in monthly manner
+  let modifiedResult = {};
+  let totalOrders = 0;
+  result.forEach((item) => {
+    const month = item.month;
+
+    // Initialize the month object if it doesn't exist
+    if (!modifiedResult[month]) {
+      modifiedResult[month] = {
+        totalPackQty: 0,
+        totalIndQty: 0,
+        totalCOG: 0,
+        totalRevenue: 0,
+        products: [],
+      };
+    }
+
+    // Accumulate values for the current month
+    modifiedResult[month].totalPackQty += item.totalPackQuantity;
+    modifiedResult[month].totalIndQty += item.totalIndividualQuantity;
+    modifiedResult[month].totalCOG += item.totalCOG;
+    modifiedResult[month].totalRevenue += item.totalRevenue;
+    totalOrders += item.totalPackQuantity;
+
+    // Add product details for the current month
+    modifiedResult[month].products.push({
+      productId: item.product,
+      qty: item.totalPackQuantity,
+      name: productNames[item.product],
+    });
   });
+  modifiedResult = {
+    totalOrders,
+    year,
+    months: modifiedResult,
+  };
+
+  res.status(StatusCodes.OK).json(modifiedResult);
+};
+
+const allTimeVendorProductsStats = async (req, res) => {
+  const { id: vendorId } = req.params;
+
+  let orderStatus = "Confirmed";
+  if (req.body.order_status) {
+    if (validOrderStatuses.includes(req.body.order_status)) {
+      orderStatus = req.body.order_status;
+    } else {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        msg: `Invalid order_status provided, please provide valid status: ${validOrderStatuses.join(
+          ", "
+        )}`,
+      });
+    }
+  }
+
+  const vendor = await Vendor.findOne({ _id: vendorId });
+  // Find all products related to the vendor
+  const products = await Product.find({ vendor: vendorId });
+  if (!products || products.length === 0) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `No products found for vendor with id ${vendorId}` });
+  }
+
+  let productNames = [];
+  let productIds = [];
+  products.forEach((product) => {
+    productNames[product._id.toString()] = product.name;
+    productIds.push(product._id);
+  });
+
+  const pipeline = [
+    {
+      $match: {
+        "cart_item.product": { $in: productIds },
+      },
+    },
+    {
+      $unwind: "$cart_item",
+    },
+    {
+      $match: {
+        "cart_item.order_status": orderStatus,
+        "cart_item.product": { $in: productIds },
+      },
+    },
+    {
+      $group: {
+        _id: {
+          product: "$cart_item.product",
+        },
+        count: { $sum: "$cart_item.quantity" }, // Sum of quantities
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        product: "$_id.product",
+        count: 1,
+      },
+    },
+    {
+      $sort: { count: -1 }, // Sort by count in descending order
+    },
+    {
+      $group: {
+        _id: null,
+        results: { $push: "$$ROOT" }, // Collect results
+        totalCount: { $sum: "$count" }, // Calculate total count
+      },
+    },
+    {
+      $lookup: {
+        from: "products", // Replace with the actual name of the product collection
+        localField: "results.product",
+        foreignField: "_id",
+        as: "productDetails",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        results: 1,
+        totalCount: 1,
+      },
+    },
+  ];
 
   const result = await Order.aggregate(pipeline);
 
-  res.status(StatusCodes.OK).json(result);
+  if (result.length === 0) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+      msg: "No product statistics found for this vendor",
+    });
+  }
+
+  // Modify the result to include count, id, and name directly under the result
+  const modifiedResult = result[0].results.map((item) => {
+    return {
+      count: item.count,
+      id: item.product,
+      name: productNames[item.product] ?? "N/A",
+    };
+  });
+
+  res.status(StatusCodes.OK).json({
+    vendorId,
+    vendorName: vendor.name,
+    orderStatus,
+    totalCount: result[0].totalCount,
+    products: modifiedResult,
+  });
 };
 
 module.exports = {
@@ -146,4 +294,5 @@ module.exports = {
   getVendorById,
   getAllProductsByVendor,
   getMonthlyProductDetails,
+  allTimeVendorProductsStats,
 };
--- api/routes/vendorRoutes.js
+++ api/routes/vendorRoutes.js
@@ -6,11 +6,13 @@ const {
   getVendors,
   getAllProductsByVendor,
   getMonthlyProductDetails,
+  allTimeVendorProductsStats,
 } = require("../controllers/vendorController");
 
 router.route("/").get(getVendors);
 router.route("/:id").get(getVendorById);
 router.route("/:id/products").get(getAllProductsByVendor);
 router.route("/:id/yearSummary/:year").get(getMonthlyProductDetails);
+router.route("/:id/allTimeStats").get(allTimeVendorProductsStats);
 
 module.exports = router;
